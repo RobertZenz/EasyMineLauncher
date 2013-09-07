dae83e0b71f7eee293bef5ca884ad547b461630a
Robert Zenz <bobby@bonsaimind.org>
2013-09-07 18:28:20 +0200

    Removed unneeded file and import.

--- src/org/bonsaimind/easyminelauncher/Main.java
+++ src/org/bonsaimind/easyminelauncher/Main.java
@@ -43,7 +43,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
